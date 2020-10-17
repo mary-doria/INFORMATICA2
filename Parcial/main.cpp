e6bb8f8af123b0c7ce60ee4dbad05bfab58ae856
Mary Doria <monadord159@gmail.com>
2020-10-17 16:17:32 -0500

    Segundo commit codigo

--- Parcial/main.cpp
+++ Parcial/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    int HO,HD,d,aO,aD,opcion;
+    int HO,HD,d,aO,aD,opcion,t,v;
 
     cout<<"Ingrese HO"<<endl;
     cin>>HO;
@@ -13,10 +13,6 @@ int main()
     cin>>HD;
     cout<<"Ingrese d"<<endl;
     cin>>d;
-    cout<<"Ingrese aO"<<endl;
-    cin>>aO;
-    cout<<"Ingrese aD"<<endl;
-    cin>>aD;
 
     cout<<"Elija una opcion"<<endl;
     cout<<"1. Generar disparos (al menos tres) ofensivos que comprometan la integridad del cañón defensivo"<<endl;
@@ -24,6 +20,24 @@ int main()
     cout<<"3. Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el cañón defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido"<<endl;
     cout<<"4. Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los cañones defensivo y ofensivo puedan ser destruidos"<<endl;
     cin>>opcion;
+
+    switch(opcion){
+        case 1:
+        cout<<"Ingrese la velocidad de la bala"<<endl;
+        cin>>v;
+        cout<<"Ingrese aO"<<endl;
+        cin>>aO;
+        //cout<<"Ingrese aD"<<endl;
+        //cin>>aD;
+        t = v*cos(aO)/d;
+        cout<<t<<endl;
+        if(HD == HO + v*sin(aO)t - t*t(9.8/2)){
+            cout<<"Colisiona"<<endl;
+        } else {
+            cout<<"No colisiona"<<endl;
+        }
+
+    }
 
     return 0;
 }
