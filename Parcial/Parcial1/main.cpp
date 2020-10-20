27118cfef1b7d302f9f829c09172f16374e1aa20
Mary Doria <monadord159@gmail.com>
2020-10-20 12:37:48 -0500

    Commit codigo parcial actualizacion

--- Parcial/Parcial1/main.cpp
+++ Parcial/Parcial1/main.cpp
@@ -1,10 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
+void generar_disparo(float HO,float HD, float d,float radio, bool ofensivo);
+
 int main()
-{
+{//EN EL OFENSIVO SERIA (0,H0) EN EL DEFENSIVO SERIA(D,HD)
     float HO,HD,d,aO,aD,y,t,v;//PARAMETROS DE ENTRADA
     int opcion;
 
@@ -24,45 +34,71 @@ int main()
 
     switch(opcion){
     case 1:{
-        cout<<"Ingrese aO(ingresa en radianes):"<<endl;//ANGULO DEL CAÑON ATACANTE
-        cin>>aO;//ENTRA EL ANGULO DEL CAÑON ATACANTE
-        int contador1=0;
-        while(contador1<3){
-            v = rand()%100+ rand()%10/10;//VELOCIDAD INICIAL DE LA BALA OFENSIVA
-            t = v*cos(aO)/d;//PARA HALLAR EL TIEMPO
-            y = HO + v*sin(aO)*t - t*t*(9.8/2);//COMPONENTE EN Y DE LA FORMULA DEL TIRO PARABOLICO
-            if(y < HD+ 0.05*d & y > HD-0.05*d){//SI LA ALTURA DE LA BALA SE ENCUENTRA ENTRE HD-0.05d y HD+0.05d
-                cout<<"Colisiona"<<endl;
-                contador1 += 1;
-                cout<<"Velocidad fue:"<<v<<endl;
-                cout<<"Tiempo es :"<<t<<endl;
-                cout<<"Y es: "<<y<<endl;//ALTURA DE LA BALA OFENSIVA
-            }
-        }}
+      generar_disparo(HO,HD,d,0.05,true);
+      }
         break;
      case 2:{
-        cout<<"Ingrese aD(ingresa en radianes):"<<endl;//ANGULO DEL CAÑON DEFENSOR
+      generar_disparo(HO,HD,d,0.025,false);
+        /*cout<<"Ingrese aD(ingresa en radianes):"<<endl;//ANGULO DEL CAÑON DEFENSOR
         cin>>aD;//ENTRA EL ANGULO DEL CAÑON DEFENSOR
         int contador1=0;
         while(contador1<3){
-            v = rand()%100 + rand()%10/10;//VELOCIDAD INICIAL DE LA BALA DEFENSIVA
-            t = v*cos(aD)/d;//PARA HALLAR EL TIEMPO
+            v = rand()%100;//VELOCIDAD INICIAL DE LA BALA DEFENSIVA
+            t = -v*cos(aD)/d;//PARA HALLAR EL TIEMPO
             y = HD + v*sin(aD)*t - t*t*(9.8/2);//COMPONENTE EN Y DE LA FORMULA DEL TIRO PARABOLICO
-            if(y < HO+ 0.05*d & y > HO-0.05*d){//SI LA ALTURA DE LA BALA SE ENCUENTRA ENTRE HD-0.05d y HD+0.05d
+            if(y < HO+ 0.025*d & y > HO-0.025*d){//SI LA ALTURA DE LA BALA SE ENCUENTRA ENTRE HO-0.025d y HO+0.025d
                 cout<<"Colisiona"<<endl;
                 contador1 += 1;
                 cout<<"Velocidad fue:"<<v<<endl;
                 cout<<"Tiempo es: "<<t<<endl;
                 cout<<"Y es :"<<y<<endl;//ALTURA DE LA BALA DEFENSIVA
             }
-        }}
+        }*/}
         break;
+      case 3:{
+
+      }
+
+    }
 
 
 
 
     }
 
+void generar_disparo(float HO,float HD, float d,float radio, bool ofensivo){
+      float a, v, t, y;
+      int contador1=0;
+      for(float i=0;i<20;i+=1){
+        //cout<<"Ingrese aO(ingresa en radianes):"<<endl;//ANGULO DEL CAÑON ATACANTE
+        //cin>>aO;//ENTRA EL ANGULO DEL CAÑON ATACANTE
+        a = (3.1416/2)*(i/20);//ANGULO DEL CAÑON ATACANTE ENTRE 0 Y 90° CON INCREMENTOS DE 5°
+        for(float j=0;j<20;j+=1){
+           v = (100/20)*j;//VELOCIDAD INICIAL DE LA BALA OFENSIVA-COGER UNA VELOCIDAD MENOR QUE 100METROS CON INCREMENTOS DE 5METROS
+          if(contador1<3){
+              t = v*cos(a)/d;//PARA HALLAR EL TIEMPO EN LA COMPONENTE X SEGUN LA FORMULA DEL MOVIEMIENTO PARABOLICO
+              y = HO + v*sin(a)*t - t*t*(9.8/2);//COMPONENTE EN Y DE LA FORMULA DEL TIRO PARABOLICO
+              //cout<<"Velocidad fue:"<<v<<endl;
+               //   cout<<"Tiempo es :"<<t<<endl;
+                //  cout<<"Y es: "<<y<<endl;
+              if(y < HD+ radio*d & y > HD-radio*d){//SI LA ALTURA DE LA BALA SE ENCUENTRA ENTRE HD-0.05d y HD+0.05d
+                  //SE DA A ENTENDER SI LA ALTURA DE LA BALA ESTA A 0.05*d O MENOS DEL CAÑON DEFENSOR
+                  cout<<"Colisiona"<<endl;
+                  contador1 += 1;
+                  cout<<"Angulo fue:"<<a<<endl;
+                  cout<<"Velocidad fue:"<<v<<endl;
+                  cout<<"Tiempo es :"<<t<<endl;
+                  cout<<"Y es: "<<y<<endl;//ALTURA DE LA BALA OFENSIVA
+              }
+          }
+          if(contador1==3){
+            break;
+          }
+        }
+        if(contador1==3){
+          break;
+        }
 
-    return 0;
+      }
+
 }
