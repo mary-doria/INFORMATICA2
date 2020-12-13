93a4988a3a243ebf1af6a3be4cc778d2322ad1a2
Mary Doria <monadord159@gmail.com>
2020-12-12 20:34:11 -0500

    Primer caso commit parcial

--- Parcial2_Informatica2/PARCIAL2_CONSOLA/cannon.h
+++ Parcial2_Informatica2/PARCIAL2_CONSOLA/cannon.h
@@ -2,12 +2,12 @@
 #define CANNON_H
 #include "objetofisico.h"
 
-
 class Cannon:public ObjetoFisico
 {
 public:
     Cannon();
     Cannon(float x_,float y_);
+    bool generarDisparo(Cannon cannon, float radio, bool ofensivo);
 };
 
 #endif // CANNON_H
--- Parcial2_Informatica2/PARCIAL2_CONSOLA/main.cpp
+++ Parcial2_Informatica2/PARCIAL2_CONSOLA/main.cpp
@@ -18,34 +18,36 @@ int main()
     cin>>d;//ENTRADA DE LA DISTANCIA AL PROGRAMA
 
     int opcion;
-        cout<<"Elija una opcion"<<endl;
-        cout<<"1. Generar disparos (al menos tres) ofensivos que comprometan la integridad del canon defensivo"<<endl;
-        cout<<"2. Generar disparos (al menos tres) defensivos que comprometan la integridad del canon ofensivo"<<endl;
-        cout<<"3. Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el canon defensivo sea destruido sin importar si el canon ofensivo pueda ser destruido"<<endl;
-        cout<<"4. Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los canones defensivo y ofensivo puedan ser destruidos"<<endl;
-        cout<<"5. Dado un disparo ofensivo efectivo y un disparo defensivo que comprometa la "<<endl;
-        cout<<"efectividad del ataque ofensivo, generar (al menos tres) disparos que neutralicen el ataque defensivo y permitan que el ataque ofensivo sea efectivo."<<endl;
-        cin>>opcion;
+    cout<<"Elija una opcion"<<endl;
+    cout<<"1. Generar disparos (al menos tres) ofensivos que comprometan la integridad del canon defensivo"<<endl;
+    cout<<"2. Generar disparos (al menos tres) defensivos que comprometan la integridad del canon ofensivo"<<endl;
+    cout<<"3. Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el canon defensivo sea destruido sin importar si el canon ofensivo pueda ser destruido"<<endl;
+    cout<<"4. Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los canones defensivo y ofensivo puedan ser destruidos"<<endl;
+    cout<<"5. Dado un disparo ofensivo efectivo y un disparo defensivo que comprometa la "<<endl;
+    cout<<"efectividad del ataque ofensivo, generar (al menos tres) disparos que neutralicen el ataque defensivo y permitan que el ataque ofensivo sea efectivo."<<endl;
+    cin>>opcion;
 
-        switch(opcion){
-        case 1:{Cannon * cannon_defensivo=new Cannon(0,HD);//Crear un puntero ,que contenga la posicion del canon defensivo
+    switch(opcion){
+    case 1:{
+        Cannon * cannon_ofensivo=new Cannon(d,HO);//Crear un puntero ,que contenga la posicion del canon ofensivo
+        Cannon * cannon_defensivo=new Cannon(0,HD);//Crear un puntero ,que contenga la posicion del canon defensivo
+        //Bullet * bullet_ofensiva=new Bullet(angulo, velocidad, 0, HD);
+        bool colisiona = cannon_ofensivo->generarDisparo(cannon_defensivo, 0.05, 1);
 
-
-
-        }
-            break;
-         case 2:{
-         ;}
-            break;
-          case 3:{
-         ;
-          }
-            break;
-        case 4:{
-          ;
-          }
-        case 5:{
-          ;
-          }
+    }
+        break;
+     case 2:{
+     ;}
+        break;
+      case 3:{
+     ;
+      }
+        break;
+    case 4:{
+      ;
+      }
+    case 5:{
+      ;
+      }
 }}
 
